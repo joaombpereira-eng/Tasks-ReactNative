18c794bb8f31bedf004ecb15f415a9e13ab4ad77
joaombpereira-eng <joaopereira.eng.tec@gmail.com>
2022-12-07 17:50:17 +0000

    Swipeable  Left and Right to Delete Task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableWithoutFeedback} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from 'react-native';
+
+import {Swipeable, GestureHandlerRootView} from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import commonStyles from '../commonStyles';
@@ -14,16 +22,41 @@ export default props => {
   const date = props.doneAt ? props.doneAt : props.estimateAt;
   const formattedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM');
 
-  return (
-    <View style={styles.container}>
-      <TouchableWithoutFeedback onPress={() => props.toogleTask(props.id)}>
-        <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
-      </TouchableWithoutFeedback>
-      <View>
-        <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
-        <Text style={styles.date}>{formattedDate}</Text>
+  const getRightContent = () => {
+    return (
+      <TouchableOpacity style={styles.right}>
+        <Icon name="trash" size={30} color="#FFF" />
+      </TouchableOpacity>
+    );
+  };
+
+  const getLeftContent = () => {
+    return (
+      <View style={styles.left}>
+        <Icon name="trash" size={20} color="#FFF" style={styles.deleteIcon} />
+        <Text style={styles.deleteText}>Apagar</Text>
       </View>
-    </View>
+    );
+  };
+
+  return (
+    <GestureHandlerRootView>
+      <Swipeable
+        renderRightActions={getRightContent}
+        renderLeftActions={getLeftContent}>
+        <View style={styles.container}>
+          <TouchableWithoutFeedback onPress={() => props.toogleTask(props.id)}>
+            <View style={styles.checkContainer}>
+              {getCheckView(props.doneAt)}
+            </View>
+          </TouchableWithoutFeedback>
+          <View>
+            <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
+            <Text style={styles.date}>{formattedDate}</Text>
+          </View>
+        </View>
+      </Swipeable>
+    </GestureHandlerRootView>
   );
 };
 
@@ -46,6 +79,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     alignItems: 'center',
     paddingVertical: 10,
+    backgroundColor: '#FFF',
   },
   checkContainer: {
     width: '20%',
@@ -76,5 +110,27 @@ const styles = StyleSheet.create({
     fontFamily: commonStyles.fontFamily,
     color: commonStyles.colors.subText,
     fontSize: 12,
+  },
+  right: {
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+  },
+  left: {
+    flex: 1,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  deleteText: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
+    margin: 10,
+  },
+  deleteIcon: {
+    marginLeft: 10,
   },
 });
