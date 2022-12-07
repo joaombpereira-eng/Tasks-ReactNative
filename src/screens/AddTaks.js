22dfafd39517f202ab8c26f51cad712e11c7c76a
joaombpereira-eng <joaopereira.eng.tec@gmail.com>
2022-12-07 15:59:57 +0000

    Added TextInput and Buttons in Modal

--- src/screens/AddTaks.js
+++ src/screens/AddTaks.js
@@ -5,10 +5,18 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
   Text,
+  TouchableOpacity,
+  TextInput,
 } from 'react-native';
 import commonStyles from '../commonStyles';
 
+const initialState = {desc: ''};
+
 export default class AddTasks extends Component {
+  state = {
+    ...initialState,
+  };
+
   render() {
     return (
       <Modal
@@ -21,6 +29,19 @@ export default class AddTasks extends Component {
         </TouchableWithoutFeedback>
         <View style={styles.container}>
           <Text style={styles.header}>Nova Tarefa</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Descrição da Tarefa..."
+            onChangeText={desc => this.setState({desc})}
+            valor={this.state.desc}></TextInput>
+          <View style={styles.buttons}>
+            <TouchableOpacity onPress={this.props.onCancel}>
+              <Text style={styles.button}>Cancelar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.button}>Guardar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
         <TouchableWithoutFeedback onPress={this.props.onCancel}>
           <View style={styles.background}></View>
@@ -36,7 +57,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
   },
   container: {
-    flex: 1,
     backgroundColor: '#FFF',
   },
   header: {
@@ -46,5 +66,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 15,
     fontSize: 18,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    margin: 20,
+    marginRight: 30,
+    color: commonStyles.colors.today,
+  },
+  input: {
+    fontFamily: commonStyles.fontFamily,
+    height: 40,
+    margin: 15,
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#E3E3E3',
+    borderRadius: 6,
   },
 });
