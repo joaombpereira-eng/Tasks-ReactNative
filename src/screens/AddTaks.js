5ad27b675ccc8ceaa2d04987ef426dc63f8e5fc5
joaombpereira-eng <joaopereira.eng.tec@gmail.com>
2022-12-07 16:42:58 +0000

    Added Task Date Input

--- src/screens/AddTaks.js
+++ src/screens/AddTaks.js
@@ -7,14 +7,47 @@ import {
   Text,
   TouchableOpacity,
   TextInput,
+  Platform,
 } from 'react-native';
+
 import commonStyles from '../commonStyles';
 
-const initialState = {desc: ''};
+import moment from 'moment';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+const initialState = {desc: '', date: new Date(), showDatePicker: false};
 
 export default class AddTasks extends Component {
   state = {
     ...initialState,
+  };
+
+  getDatePicker = () => {
+    let datePicker = (
+      <DateTimePicker
+        value={this.state.date}
+        onChange={(_, date) => this.setState({date, showDatePicker: false})}
+        mode="date"
+      />
+    );
+
+    const dateString = moment(this.state.date).format(
+      'ddd, D [de] MMMM [de] YYYY',
+    );
+
+    if (Platform.OS === 'android') {
+      datePicker = (
+        <View>
+          <TouchableOpacity
+            onPress={() => this.setState({showDatePicker: true})}>
+            <Text style={styles.date}>{dateString}</Text>
+          </TouchableOpacity>
+	  {this.state.showDatePicker && datePicker}
+        </View>
+      );
+    }
+
+    return datePicker;
   };
 
   render() {
@@ -33,7 +66,9 @@ export default class AddTasks extends Component {
             style={styles.input}
             placeholder="Descrição da Tarefa..."
             onChangeText={desc => this.setState({desc})}
-            valor={this.state.desc}></TextInput>
+            valor={this.state.desc}
+          />
+          {this.getDatePicker()}
           <View style={styles.buttons}>
             <TouchableOpacity onPress={this.props.onCancel}>
               <Text style={styles.button}>Cancelar</Text>
@@ -84,5 +119,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#E3E3E3',
     borderRadius: 6,
+  },
+  date: {
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 20,
+    marginLeft: 15
   },
 });
