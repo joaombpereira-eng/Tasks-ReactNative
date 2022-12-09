8cb840684cdb2a1b7bf1a119d4619959bc0e1988
joaombpereira-eng <joaopereira.eng.tec@gmail.com>
2022-12-09 13:54:01 +0000

    Authentication Integrated w/ Backend - Signup

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -8,25 +8,48 @@ import {
   Alert,
 } from 'react-native';
 
+import axios from 'axios';
+
 import commonStyles from '../commonStyles';
 import backgroundImage from '../../assets/imgs/login.jpg';
 
 import AuthInput from '../components/AuthInput';
+import {server, showError, showSuccess} from '../common';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  stageNew: false,
+};
 
 export default class Auth extends Component {
   state = {
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    stageNew: false,
+    ...initialState,
   };
 
   signinOrSignup = () => {
     if (this.state.stageNew) {
-      Alert.alert('Sucesso', 'Criar Conta');
+      this.signup();
     } else {
       Alert.alert('Sucesso', 'Logar');
+    }
+  };
+
+  signup = async () => {
+    try {
+      await axios.post(`${server}/signup`, {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+        confirmPassword: this.state.confirmPassword,
+      });
+
+      showSuccess('Utilizador Registado!');
+      this.setState({...initialState});
+    } catch (e) {
+      showError(e);
     }
   };
 
@@ -68,6 +91,7 @@ export default class Auth extends Component {
               placeholder="Confirmar Password"
               value={this.state.confirmPassword}
               style={styles.input}
+              secureTextEntry={true}
               onChangeText={confirmPassword => this.setState({confirmPassword})}
             />
           )}
