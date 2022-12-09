d2b2ee897d029205a730de1c8f70e92cdf25984e
joaombpereira-eng <joaopereira.eng.tec@gmail.com>
2022-12-09 11:22:35 +0000

    Authentication Screen Improved

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,14 +1,46 @@
 import React, {Component} from 'react';
-import {Text, ImageBackground, StyleSheet} from 'react-native';
+import {
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 
 import commonStyles from '../commonStyles';
 import backgroundImage from '../../assets/imgs/login.jpg';
 
 export default class Auth extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
   render() {
     return (
       <ImageBackground source={backgroundImage} style={styles.background}>
         <Text style={styles.title}>Tasks</Text>
+        <View style={styles.formContainer}>
+          <TextInput
+            placeholder="E-mail"
+            value={this.state.email}
+            style={styles.input}
+            onChangeText={email => this.setState({email})}
+          />
+          <TextInput
+            placeholder="Password"
+            value={this.state.password}
+            style={styles.input}
+            onChangeText={password => this.setState({password})}
+          />
+          <TouchableOpacity>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Login</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
     );
   }
@@ -26,5 +58,26 @@ const styles = StyleSheet.create({
     width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  formContainer: {
+    backgroundColor: 'rgba(0,0,0,0.8)',
+    padding: 20,
+    width: '90%',
+  },
+  input: {
+    marginTop: 10,
+    backgroundColor: '#FFF',
+    padding: Platform.OS === 'ios' ? 15 : 10,
+  },
+  button: {
+    backgroundColor: '#080',
+    marginTop: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
   },
 });
