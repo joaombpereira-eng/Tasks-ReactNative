e15c9c92a696a97e4ba3cc7fe26d7d85c9e22c41
joaombpereira-eng <joaopereira.eng.tec@gmail.com>
2022-12-09 14:03:06 +0000

    Authentication Integrated w/ Backend - Signin

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -33,7 +33,7 @@ export default class Auth extends Component {
     if (this.state.stageNew) {
       this.signup();
     } else {
-      Alert.alert('Sucesso', 'Logar');
+      this.signin();
     }
   };
 
@@ -48,6 +48,22 @@ export default class Auth extends Component {
 
       showSuccess('Utilizador Registado!');
       this.setState({...initialState});
+    } catch (e) {
+      showError(e);
+    }
+  };
+
+  signin = async () => {
+    try {
+      const res = await axios.post(`${server}/signin`, {
+        email: this.state.email,
+        password: this.state.password,
+      });
+
+      axios.defaults.headers.common[
+        'Authorization'
+      ] = `bearer ${res.data.token}`;
+      this.props.navigation.navigate('Home');
     } catch (e) {
       showError(e);
     }
